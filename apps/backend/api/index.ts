f78e34076456e5f198159b6cf84e056f79f63853
Sayed Sinan Umer Jifry <sayedsinan7@gmail.com>
2026-05-14 10:57:27 +0530

    updated

--- apps/backend/api/index.ts
+++ apps/backend/api/index.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from '../src/app.module';
-import * as express from 'express';
+import express = require('express');
 
 const server = express();
 let cachedApp: any;
